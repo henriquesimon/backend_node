be1cfe07374ae87bbd964e849a5f1b64aecf3611
Henrique Simon <henriquesimon1998@gmail.com>
2025-03-12 22:48:30 -0300

    updated server.js and .env.example files

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ import privateRoutes from "./routes/private.js";
 import auth from "./middlewares/auth.js";
 
 const app = express();
+const PORT = process.env.PORT
 
 app.use(express.json());
 app.use("/", publicRoutes);
 app.use("/", auth, privateRoutes);
 
-app.listen(3000, () => console.log("Servidor rodando."));
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}.`));
